2fd3f374999b9a3f3824005516316827b6f8541a
MohanManirul <fiforeg@gmail.com>
2022-05-21 12:06:32 +0600

    console error solve

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ var app = Vue.createApp({
     data() {
         return {
             msg : "first app created"
-        }
+        };
     },
-
     methods: {
         handleInput(e){
             console.log(e);
         }
-    }
+    },
 
-    
 });
 
 app.mount("#app");
