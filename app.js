eedb1affbe733ab2529f6615da17e5257f486bdc
MohanManirul <fiforeg@gmail.com>
2022-05-21 11:38:20 +0600

    console log

--- app.js
+++ app.js
@@ -1,57 +1,17 @@
-
-
-// var app = Vue.createApp({
-//     data() {
-//         return {
-//             count:0
-//         };
-//     },
-    
-//     // method declaratio 
-//     // methods: {
-//     //     increase() {
-//     //         //this.count = this.count + 1;
-//     //         this.count++;
-//     //     },
-
-//     //     decrease() {
-//     //        // this.count = this.count - 1;
-//     //         this.count--;
-//     //     },
-//     // },
-    
-//     // methods argument 
-//     methods: {
-//             increase(amount) {
-//                 this.count = this.count + amount;
-//                 //this.count++;
-//             },
-    
-//             decrease(amount) {
-//                this.count = this.count - amount;
-//                 //this.count--;
-//             },
-//             test(e){
-//                 console.log(e);
-//             }
-//         },
-
-// });
-
-// app.mount("#app");
-
 var app = Vue.createApp({
     data() {
         return {
-            
-        };
+            msg : "first app created"
+        }
     },
 
     methods: {
-         handleKeyup(e){
+        handleInput(e){
             console.log(e);
-         }
-    },
+        }
+    }
+
+    
 });
 
 app.mount("#app");
