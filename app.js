62b7d72eddddf1f43fdcd230a0d8e05e81afcd43
MohanManirul <fiforeg@gmail.com>
2022-05-21 13:57:36 +0600

    class binding

--- app.js
+++ app.js
@@ -1,32 +1,13 @@
 var app = Vue.createApp({
     data() {
         return {
-            num : "", //property 
-            result : "", //property 
+            activeClass: "red"
         };
     },
     methods: {
-        handleInput(e){
-            console.log(e);
-            this.num = e.target.value;
-        },
-
-        getDouble(){
-            this.result = this.num * 2 ;
-        },
-    
-        getSquare(){
-            this.result = this.num * this.num ;
-        },
-        
-        addTen(){
-            this.result = this.num + 10 ;
-        },
-    
-        getReset(){
-            this.num = "";
-            this.result = "";
-        },
+        setClass(ClassName){
+            this.activeClass = ClassName;
+        }
     },
 
     
