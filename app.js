8c5a63de5fedbb6799bc4f6099a07dab5b30a237
MohanManirul <fiforeg@gmail.com>
2022-05-21 12:22:14 +0600

    double square reset calculator app

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 var app = Vue.createApp({
     data() {
         return {
-            msg : "first app created"
+            num : "", //property 
+            result : "", //property 
         };
     },
     methods: {
         handleInput(e){
             console.log(e);
-        }
+            this.num = e.target.value;
+        },
+
+        getDouble(){
+            this.result = this.num * 2 ;
+        },
+    
+        getSquare(){
+            this.result = this.num * this.num ;
+        },
+    
+        getReset(){
+            this.num = "";
+            this.result = "";
+        },
     },
+
+    
 
 });
 
