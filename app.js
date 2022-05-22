c1f4a90e591d42e5c281d9c640b0cdabaac96a16
MohanManirul <fiforeg@gmail.com>
2022-05-22 10:47:27 +0600

    v-i v-else-if v-else

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var app = Vue.createApp({
     data() {
         return {
-            show:false
+            show:false,
+            skills:[
+                {name : "html" , experience:5},
+                {name : "css" , experience:2},
+                {name : "javascript" , experience:9},
+                {name : "php" , experience:5},
+                {name : "sql" , experience:6},
+                {name : "laravel" , experience:3},
+                {name : "vue" , experience:2}
+            ]
         };
     },
     
