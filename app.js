8905bc5345b8cfefc7f8f5383df4a0ed0f436c2d
MohanManirul <fiforeg@gmail.com>
2022-05-24 10:32:10 +0600

    vue template

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.component('contact-details' , {
         <h2>Contact Details</h2>
         <p><strong>Name:</strong>Mamunur Rashid</p>
         <p><strong>Web:</strong>www.mamunbd.com</p>
-        <p><strong>Address:</strong>Dhaka , Bangladesh</p>
     `
 });
 
