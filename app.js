24923099acb751c95192b88eb303c8ff2e999729
MohanManirul <fiforeg@gmail.com>
2022-05-31 16:41:38 +0600

    array object list rendering in array object

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 var app = Vue.createApp({
     data() { 
         return {
-           skills : ["HTML" , "CSS" , "JS"],
-           newSkill :""
+           skills : [
+               {name: "HTML" , experience : 1},
+               {name: "css" , experience : 2},
+               {name: "js" , experience : 3},
+               {name: "php" , experience : 4},
+               {name: "laravel" , experience : 5}
+           ],
+           newSkill : {name  : "" , experience:0}
         };
     },
     methods: {
         addSkill(){
             this.skills.push(this.newSkill);
-            this.newSkill = ""
+            this.newSkill = {}
         },
         removeSkill(i){
             this.skills.splice(i , 1);
