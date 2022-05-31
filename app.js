d920dd49dcf888aeaef259a72f3470f249ed14bd
MohanManirul <fiforeg@gmail.com>
2022-05-31 11:33:04 +0600

    argument in methods

--- app.js
+++ app.js
@@ -1,42 +1,19 @@
-var app1 = Vue.createApp({
+var app = Vue.createApp({
     data() { 
         return {
-<<<<<<< HEAD
-           name: "manirul"
-        };
-    },
-    template : `
-        <h2>Enter your name</h2>
-        <p>{{ name }}</p>
-    `
-  
-});
-
-
-app1.mount("#app1");
-
-var app2 = Vue.createApp({
-    data(){
-        return {
-            messege : "Hello World"
-        }
-    },
-    template  : " <h3>{{ messege }}</h3>"
-=======
             msg : "Counter App" ,
             count : 0
         };
     },
     methods: {
-        increase() {            
-           
+        increase(amount) {            
+           this.count= this.count + amount ;
         },
-        decrease() {
-            this.count-- ;
+        decrease(amount) {
+            this.count= this.count - amount ;
         }
     },
   
->>>>>>> origin/mohan
 });
 
-app2.mount("#app2");
+app.mount("#app");
