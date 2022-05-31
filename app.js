0efd4400e0d03f007680312e80327c10f0dd924e
MohanManirul <fiforeg@gmail.com>
2022-05-31 15:00:20 +0600

    v-model two way data bindings

--- app.js
+++ app.js
@@ -6,19 +6,17 @@ var app = Vue.createApp({
         };
     },
     methods: {
-        // received input value as num 
-        handleInput(e){
-            this.num = e.target.value;
-        },
-
+        
         double(){
             this.result = this.num * 2 ;
+           
         },
         squire(){
             this.result = this.num * this.num ;
         },
         reset(){
-            this.result = this.num * 0 ;
+            this.num = "" ;
+            this.result = "" ;
         }
 
     },
